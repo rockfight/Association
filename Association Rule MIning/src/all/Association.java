dd81dfc4aea3af79146fd7a6985934fc671d7757
prasanna <prasanna@localhost.localdomain>
2014-04-24 15:43:33 +0545

    Fininshed the 3 Classes

--- Association Rule MIning/src/all/Association.java
+++ Association Rule MIning/src/all/Association.java
@@ -1,15 +1,35 @@
 package all;
 
+
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 
 
 public class Association {
 
-	public static void main(String[] args) {
-		AndEr ander = new AndEr();
-		IntWritable int1 = new IntWritable(7);
-		IntWritable int2 = new IntWritable(9);
-		ander.map(int1,int2);
+	public static void main(String[] args) throws Exception{
+		if (args.length != 2){
+			System.err.println("Usuage: <Input Path> <Output Path>");
+			System.exit(-1);
+		}
+		Job job= new Job();
+		job.setJarByClass(Association.class);
+		job.setJobName("Association");
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileInputFormat.addInputPath(job, new Path (args[1]));
+		
+		job.setMapperClass(AndingMapper.class);
+		job.setReducerClass(LargeItemReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0:1);
+		
 
 	}
 
--- Association Rule MIning/src/all/LargeItemReducer.java
+++ Association Rule MIning/src/all/LargeItemReducer.java
@@ -6,15 +6,19 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class LargeItemReducer extends Reducer<Text,IntWritable,Text,Text> {
+public class LargeItemReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 	
 	@Override
 	public void reduce(Text key, Iterable<IntWritable> values, Context context) 
 			throws IOException, InterruptedException{
 		int noOfOne = 0;
+		int totalOnes=0;
 		for (IntWritable value : values){
-			
+			noOfOne=Integer.bitCount(value.get());
+			totalOnes = totalOnes + noOfOne;
 		}
+	
+		context.write(key, new IntWritable(totalOnes));
 		
 	}
 
