2fda13fabbf08d4e083e3fa89b419891b65c358e
prasanna <prasanna@localhost.localdomain>
2014-04-30 22:20:03 +0545

    New hadoop 1.2.1

--- Association Rule MIning/src/all/Association.java
+++ Association Rule MIning/src/all/Association.java
@@ -1,36 +1,37 @@
 package all;
 
 
+import org.apache.hadoop.conf.Configuration;
+//import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+//import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+//import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+//import org.apache.hadoop.util.Tool;
+//import org.apache.hadoop.util.ToolRunner;
 
 
-public class Association {
-
-	public static void main(String[] args) throws Exception{
-		if (args.length != 2){
-			System.err.println("Usuage: <Input Path> <Output Path>");
-			System.exit(-1);
-		}
-		Job job= new Job();
-		job.setJarByClass(Association.class);
-		job.setJobName("Association");
-		
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileInputFormat.addInputPath(job, new Path (args[1]));
-		
-		job.setMapperClass(AndingMapper.class);
-		job.setReducerClass(LargeItemReducer.class);
-		
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		
-		System.exit(job.waitForCompletion(true) ? 0:1);
-		
-
+public class Association  {
+	
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration() ;
+		Job job = new Job(conf, "ExampleJob") ;
+		job.setJarByClass(Association.class) ;
+		job.setMapperClass(AndingMapper.class) ;
+		job.setReducerClass(LargeItemReducer.class) ;
+		job.setOutputKeyClass(Text.class) ;
+		job.setOutputValueClass(IntWritable.class) ;
+		KeyValueTextInputFormat.addInputPath(job, new Path(args[0])) ;
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+		// Execute the job and wait for it to complete
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
-
 }
+
+	
